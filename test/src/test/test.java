c0043acd42c7ac5e378778d2fd132998661a7d60
acala <acala@DESKTOP-JLVIICQ>
2020-11-08 23:11:32 +0900

    1

---
+++ test/src/test/test.java
@@ -0,0 +1,21 @@
+package test;
+
+import java.math.BigDecimal;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+//		String test = "3000127767";
+		String test = "2147483648";
+		int a = Integer.parseInt(test);
+//		BigDecimal b = new BigDecimal(test);
+		System.out.println(a);
+//		System.out.println(b);
+		
+	}
+
+}
